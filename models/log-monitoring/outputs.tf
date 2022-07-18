d745d9e1ec5637f64d83a681dd129405b27455fd
InsafSalmoun <i.salmoun34@gmail.com>
2022-07-18 16:21:59 +0200

    v2

--- models/log-monitoring/outputs.tf
+++ models/log-monitoring/outputs.tf
@@ -1,6 +1,15 @@
 output "server_ip_addr" {
-  value = aws_instance.server.public_ip
+  value = aws_instance.kibana
 }
-output "server_ip_dns" {
-  value = aws_instance.server.public_dns
+
+output "server_ip_addr" {
+  value = aws_instance.server.node1_elk
 }
+
+output "server_ip_addr" {
+  value = aws_instance.server.node2_elk
+}
+
+output "server_ip_addr" {
+  value = aws_instance.server.logstash
+  }
